6f22285a10bb1c10f4f5666d3b58285535a1ac79
jopoepl <joel_joseph93@outlook.com>
2022-08-31 13:50:41 +0530

    Assgn III updated

--- block-BNaadn/bookstore/models/category.js
+++ block-BNaadn/bookstore/models/category.js
@@ -3,7 +3,7 @@ const book = require("./book")
 var Schema = mongoose.Schema
 
 var categorySchema = new Schema({
-    name: [{type: String, required: true}],
+    name: {type: String, required: true},
     books: [{type: Schema.Types.Object, ref: "book"}]
 }, {timestamps: true})
 
